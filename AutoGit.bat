a5c8d69d54bf09ed78707968e1c5105a635609e3
Aditya <adpandeyindia1@gmail.com>
2024-08-02 18:17:07 +0530

    Remove .bat files and AutoGit.py from repository

--- AutoGit.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Users\adpan\AppData\Local\Programs\Python\Python312\python.exe" "X:\Program files\Projects\AutoGit.py"
-pause
--- AutoGit.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def open_powershell():
-    subprocess.Popen(['powershell', '-Command', 'git status; git add .; git status; git commit -m "Date $(Get-Date -Format yyyy-MM-dd)"; git status; git push origin master; git status; clear-history'])
-
-# Example usage:
-if __name__ == "__main__":
-    open_powershell()
